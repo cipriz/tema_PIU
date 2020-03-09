98a1f13a5af89bf8411a66d297a794b5958a8649
Ciprian Ciotir <ciotirc@gmail.com>
2020-03-09 21:45:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student s1 = new Student("Ciotir","Ciprian","3122B",5);
+            Student s2 = new Student("test", "test2", "another test", 4);
+            Console.WriteLine(s1.Info());
+            Console.WriteLine(s2.Info());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema
+{
+    class Student
+    {
+        string Nume;
+        string Prenume;
+        string Grupa;
+        int Nota;
+        string Promovat;
+        public Student()
+        {
+            Nume = string.Empty;
+            Prenume = string.Empty;
+            Grupa = string.Empty;
+            Nota = 0;
+            Promovat = "nu";
+        }
+        public Student(string _Nume,string _Prenume,string _Grupa,int _Nota)
+        {
+            Nume = _Nume;
+            Prenume = _Prenume;
+            Grupa = _Grupa;
+            Nota = _Nota;
+            if (Nota >= 5)
+                Promovat = "da";
+            else Promovat = "nu";
+        }
+        public string Info()
+        {
+            return string.Format("Nume: {0}\nPrenume: {1}\nGrupa: {2}\nNota: {3}\nPromovat: {4}\n", Nume, Prenume, Grupa, Nota, Promovat);
+        }
+    }
+}
